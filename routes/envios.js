18e30895ba79b9ac2fe1221dfb555bbbbe3e43cf
Daniel Salazar <jdps106@hotmail.com>
2022-12-07 13:42:06 -0500

    se arregla query de actualizar

--- routes/envios.js
+++ routes/envios.js
@@ -1,4 +1,5 @@
 const express = require("express");
+const env = require("../models/env");
 const router = express.Router();
 const Envios = require("../models/env");
 
@@ -22,7 +23,7 @@ router.get("/allEnvs", (req, res) => {
   });
 });
 
-//?----localhost:6000/env/allEnvs
+//?----localhost:6000/env/envByid/???
 router.get("/envByid/:id", (req, res) => {
   const { id } = req.params;
 
@@ -33,6 +34,40 @@ router.get("/envByid/:id", (req, res) => {
       res.send(envio);
     }
   });
+});
+
+//?----localhost:6000/env/update/???
+router.put("/update/:id", (req, res) => {
+ const {id}=req.params;
+    const {
+    fecha,
+    largo,
+    ancho,
+    alto,
+    peso,
+    direccionR,
+    ciudadR,
+    nombredestinatario,
+    cedula,
+    direccionE,
+    ciudadE,
+    estado
+  } = req.body;
+  Envios.updateOne({_id:id},{$set:
+   { fecha,
+    largo,
+    ancho,
+    alto,
+    peso,
+    direccionR,
+    ciudadR,
+    nombredestinatario,
+    cedula,
+    direccionE,
+    ciudadE,
+    estado}
+  }).then((env)=>{res.json(env)}).catch((error)=>res.json({msg:"ocurrio algo malo"}));
+  
 });
 
 //?----localhost:6000/env/delete/???
